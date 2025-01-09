c872f8bab260fbb2dbb257d0300724148bc1cd8e
Administrator Cyn <grieferjesus27@gmail.com>
2025-01-08 16:48:41 -0800

    ok

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { invoke } from "@tauri-apps/api/core";
+//React!
+
+async function calcParts(){
+
+}
+window.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("tsInput");
+  if (button) {
+    button.addEventListener("click", async () => {
+      button.innerHTML = "Clicked";
+    });
+  }
+});
