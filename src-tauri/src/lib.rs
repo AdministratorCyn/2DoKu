34e798cca0001bf15561f054ad514ec45d8e4689
Administrator Cyn <grieferjesus27@gmail.com>
2025-01-17 09:03:29 -0800

    started solving

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,33 +1,104 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+#[derive(serde::Serialize)]
+struct Sudoku{
+    char_grid: [[i8; 9]; 9],
+    cand_grid: Vec<Vec<bool>>
 }
-use std::time::{Instant};
-#[tauri::command]
-fn count() -> i32 {
-    let mut count = 0;
-    let start = Instant::now();
-    for _ in 0..1000000 {
-        count += 1;
-    }
-    let elapsed = start.elapsed();
-    return (elapsed.as_micros() as i32);
-}
-struct Batch;
 
-impl Batch {
-    pub fn new() -> Self {
-        Batch
+impl Sudoku {
+    fn new(str: &str) -> Self {
+        let mut char_grid = [[0; 9]; 9];
+        for (i, c) in str.chars().enumerate() {
+            if c != '.' {
+                char_grid[i / 9][i % 9] = c.to_digit(10).unwrap() as i8;
+            }
+        }
+        Sudoku {
+            char_grid,
+            cand_grid: vec![vec![true; 81]; 9],
+        }
     }
-    
+    fn autocand(&mut self) {
+        for i in 0..81 {
+            if self.char_grid[i / 9][i % 9] != 0 {
+                for j in 0..9 {
+                    if j != i % 9{
+                        self.cand_grid[i / 9][j % 9 * 9 + self.char_grid[i / 9][i % 9] as usize - 1] = false;
+                    }
+                    if j != i / 9{
+                        self.cand_grid[j][i % 9 * 9 + self.char_grid[i / 9][i % 9] as usize - 1] = false;
+                    }
+                    //box
+                    if j != (i % 3 + (i / 9) % 3 * 3) {
+                        self.cand_grid[(i / 9) / 3 * 3 + j / 3][((i % 9) / 3 * 27) + (j % 3 * 9) + self.char_grid[i / 9][i % 9] as usize - 1] = false;
+                    }
+                    //cell
+                    if j != (self.char_grid[i / 9][i % 9] as usize - 1) {
+                        self.cand_grid[i / 9][i % 9 * 9 + j] = false;
+                    } 
+                }
+            }
+        }
+    }
+    fn ns(&mut self) {
+        let mut count: i32 = 0;
+        let mut i = 0;
+        while i < 729 {
+            if i % 9 == 0 && self.char_grid[i / 81][i % 81 / 9] != 0 {  
+                count = 0;
+                i += 8;
+            }
+            else {
+                if count > 1 {
+                    count = 0;
+                    i = i / 9 * 9 + 8;
+                }
+                else if self.cand_grid[i / 81][i % 81] {
+                    count += 1;
+                }
+                else if i % 9 == 8 && count != 1 {
+                    count = 0;
+                }
+                else if i % 9 == 8 {
+                    for j in 0..9 {
+                        if self.cand_grid[i / 81][i % 81 - 8 + j] {
+                            self.char_grid[i / 81][i % 81 / 9] = (j + 1) as i8;
+                            return;
+                        }
+                    }
+                }
+            }
+            i += 1;
+        }
+    }
+}
+use std::{string, time::{Duration, Instant}};
+#[derive(serde::Serialize)]
+struct Output{
+    time: String,
+    puzzle: Sudoku
+}
+
+//we only need chars for autocand, can do bitwise or something faster? after
+//bitvec but more compressed
+#[tauri::command]
+fn solve(grid: &str) -> Output {
+    let mut grid = Sudoku::new(grid);
+    println!("{:?}", grid.char_grid);
+    let start = Instant::now();
+    for _ in 0..100 {
+        grid.ns();
+        grid.autocand(); //autocand is 5981ms avg alone, 91.7% of total time
+    }
+    let time = start.elapsed().as_micros().to_string();
+    return Output{time, puzzle: grid}; //struct for both grids
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, count])    
+        .invoke_handler(tauri::generate_handler![solve])    
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/batch/batch.ts
+++ src/batch/batch.ts
@@ -1,13 +1,19 @@
 import { invoke } from '@tauri-apps/api/core';
 const invoke = window.__TAURI__.core.invoke;
 document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById("starts");
+  const button = document.getElementById("start");
   button?.addEventListener("click", async () => {
     try {
+      let serverTime;
       let start = new Date().getMilliseconds();
-      const serverTime = await invoke('count');
+      const response = await invoke('solve', { grid: "9.486.53..7...5.9.6...2......9....64....9....71....3......4...1.6.1...4..48.532.6"});
+      serverTime = response.time;
+      let cand_grid: boolean[][] = response.puzzle.cand_grid;
+      let char_grid: boolean[][] = response.puzzle.char_grid;
       let end = new Date().getMilliseconds();
-      button.innerHTML = serverTime + " ms server only, " + (end - start) + "ms total";
+      button.innerHTML = serverTime + " µs server only, " + (end - start) + "ms total";
+      console.log(cand_grid);
+      console.log(char_grid);
     } catch (error) {
       console.error('Error invoking greet:', error);
     }
