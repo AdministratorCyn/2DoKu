96e65041e7bedd3effaa4da3b9cdbc54cfa4c545
Administrator Cyn <grieferjesus27@gmail.com>
2025-01-27 06:39:44 -0800

    pushing before i mess things up <3

--- src/batch/batch.ts
+++ src/batch/batch.ts
@@ -13,6 +13,7 @@ for (let i = 0; i < 9; i++) {
     }
   }
 }
+
 document.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById("start");
   button?.addEventListener("click", async () => {
@@ -34,45 +35,74 @@ document.addEventListener('DOMContentLoaded', () => {
   const showcase = document.getElementById("showcase");
   showcase ? initComp(showcase) : null;
 });
-function initComp(parent: HTMLElement) {
+async function initComp(parent: HTMLElement) {
   for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-          const cell = document.createElement("div");
-          if (char_grid[i][j] != 0) {
-            const button = document.createElement("button");
-            button.classList.add('sc');
-            button.addEventListener('click', () => {
-
-            });
-            cell.appendChild(button);
-          }
-          else {
-            for (let k = 0; k < 9; k++) {
-              const button = document.createElement("button");
-              cell.classList.add('ca');
-              button.classList.add('cand');
-              button.innerHTML = (k + 1).toString();
-              button.addEventListener('click', () => {
-                button.innerHTML ? button.innerHTML = '' : button.innerHTML = (k + 1).toString();
-              });
-                button.addEventListener('keydown', (event) => {
-                if (event.key >= '1' && event.key <= '9') {
-                  while (cell.firstChild) {
-                    cell.removeChild(cell.firstChild);
-                  }
-                  cell.classList.remove('ca');
-                  const button = document.createElement("button");
-                  button.classList.add('sc');
-                  cell.appendChild(button); 
-                }
-                });
-              cell.appendChild(button);
-            }
-          }
-          parent.appendChild(cell);
+    for (let j = 0; j < 9; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add('cell');
+      if (i % 3 === 2 && i != 8) {
+        cell.style.borderBottom = '2px solid white';
       }
+      if (j % 3 === 0 && j !== 0) {
+        cell.style.borderLeft = '2px solid white';
+        cell.style.padding = '0px';
+      }
+      if (char_grid[i][j] != 0) {
+        sc(cell, puzzle.charAt(i * 9 + j) != '.', i, j);
+      } else {
+        usc(cell, i, j);
+      }
+      parent.appendChild(cell);
     }
+  }
 }
-function update(parent: HTMLElement, pos: number) {
-
+//cell extends html element {}
+function sc(cell: HTMLDivElement, given: boolean, i: number, j: number) {
+  const button = document.createElement("button");
+  cell.classList.remove("sc", "ca");
+  button.classList.add('sc');
+  if (given) {
+    button.style.color = "67bbe5";
+  }
+  else {
+    button.addEventListener('keydown', (event) => {
+      if (event.key === 'Backspace' || event.key === 'Delete') {
+        clearDiv(cell);
+        usc(cell, i, j);
+      }
+    });
+  }
+  button.innerHTML = char_grid[i][j].toString();
+  cell.appendChild(button);
+}
+function usc(cell: HTMLDivElement, i: number, j: number) {
+  cell.classList.remove("sc", "ca");
+  cell.classList.add("ca");
+  for (let i = 0; i < 9; i++) {
+    const button = document.createElement("button");
+    button.innerHTML = (i + 1).toString();
+    button.classList.add("cand");
+    button.addEventListener('click', () => {
+      button.innerHTML !== '' ? button.innerHTML = '' : button.innerHTML = (i + 1).toString();
+    });
+    button.addEventListener('keydown', (event) => {
+      if (event.key >= '1' && event.key <= '9') {
+        char_grid[i][j] = parseInt(event.key);
+        clearDiv(cell);
+        sc(cell, false, i, j);
+      }
+    });
+    button.addEventListener('mouseover', () => {
+      button.style.color = 'white';
+    });
+    button.addEventListener('mouseout', () => {
+      button.style.color = 'c0b5cb'
+    });
+    cell.appendChild(button);
+  }
+}
+function clearDiv(cell: HTMLDivElement) {
+  while(cell.firstChild) {
+    cell.removeChild(cell.firstChild);
+  }
 }
